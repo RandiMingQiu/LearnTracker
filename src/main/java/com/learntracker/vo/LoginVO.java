2ba5e357bf3a0c35305acd67542b9db046d442de
RandiMingQiu <3092595013@qq.com>
2026-04-07 16:15:18 +0800

    修复了token返回的问题

--- src/main/java/com/learntracker/vo/LoginVO.java
+++ src/main/java/com/learntracker/vo/LoginVO.java
@@ -1,11 +1,12 @@
 package com.learntracker.vo;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class LoginVO {
-
     private String token;
+
+    public LoginVO(String token) {
+        this.token = token;
+    }
 }
